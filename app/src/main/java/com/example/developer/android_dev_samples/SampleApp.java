f092a572dbb0d72a665df8ed2b0d1d02b6ed8214
kalaiselvan <kalaiselvan.1@tcs.com>
2016-09-26 19:32:19 +0530

    android-folder-structure

---
+++ app/src/main/java/com/example/developer/android_dev_samples/SampleApp.java
@@ -0,0 +1,14 @@
+package com.example.developer.android_dev_samples;
+
+import android.app.Application;
+
+/**
+ * Created by developer on 26/9/16.
+ */
+public class SampleApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
