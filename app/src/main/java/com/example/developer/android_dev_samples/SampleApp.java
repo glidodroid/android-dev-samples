b86c8e82c63e643786455c1a05fe745a55f93c92
kalaiselvan <kalaiselvan.1@tcs.com>
2016-09-29 22:20:34 +0530

    Added libraries in gradle file.

--- app/src/main/java/com/example/developer/android_dev_samples/SampleApp.java
+++ app/src/main/java/com/example/developer/android_dev_samples/SampleApp.java
@@ -2,13 +2,19 @@ package com.example.developer.android_dev_samples;
 
 import android.app.Application;
 
+import com.facebook.drawee.backends.pipeline.Fresco;
+
+import timber.log.Timber;
+
 /**
- * Created by developer on 26/9/16.
+ * Sample app application class.
  */
 public class SampleApp extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Timber.plant(new Timber.DebugTree());
+        Fresco.initialize(this);
     }
 }
---
+++ code_verification.sh
@@ -0,0 +1,1 @@
+./gradlew :app:check --stacktrace
