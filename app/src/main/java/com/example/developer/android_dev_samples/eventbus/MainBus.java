ba3cdc4d5ffaeadaafd28536fbd245dff22f81c0
kalaiselvan <kalaiselvan.1@tcs.com>
2016-11-11 23:44:24 +0530

    Event bus added using rx java.

---
+++ app/src/main/java/com/example/developer/android_dev_samples/eventbus/MainBus.java
@@ -0,0 +1,21 @@
+package com.example.developer.android_dev_samples.eventbus;
+
+/**
+ * Created by Airbase on 08-Oct-16.
+ */
+
+public class MainBus extends RxEventBus {
+
+    private static MainBus instance;
+
+    public static MainBus getInstance() {
+        if (instance==null) {
+            instance =new MainBus();
+        }
+        return instance;
+    }
+
+    private MainBus() {
+
+    }
+}
