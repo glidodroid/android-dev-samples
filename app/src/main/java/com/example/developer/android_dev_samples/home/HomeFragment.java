68e923ddf4ebb3392bff31c7d8ff0b7f366f44a3
kalaiselvan <kalaiselvan.1@tcs.com>
2018-01-19 16:32:37 +0530

    Child frag with dagger2 reference.

--- app/src/main/java/com/example/developer/android_dev_samples/home/HomeFragment.java
+++ app/src/main/java/com/example/developer/android_dev_samples/home/HomeFragment.java
@@ -2,6 +2,7 @@ package com.example.developer.android_dev_samples.home;
 
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,26 +12,46 @@ import com.example.developer.android_dev_samples.R;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.Unbinder;
+import dagger.android.AndroidInjection;
+import dagger.android.AndroidInjector;
+import dagger.android.DispatchingAndroidInjector;
+import dagger.android.HasFragmentInjector;
 import timber.log.Timber;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class HomeFragment extends Fragment implements HomeView {
+public class HomeFragment extends Fragment implements HasFragmentInjector, HomeView {
 
+    private static final String FRAG_TAG_HOME_CHILD = "homeChild";
 
     @Inject
     public HomeFragment() {
         // Required empty public constructor
     }
 
+    //@Inject
+    //HomePresenterImpl<HomeView> presenter;
+
     @Inject
-    HomePresenterImpl<HomeView> presenter;
+    DispatchingAndroidInjector<Fragment> fragmentInjector;
+
+    @Inject
+    HomeChildFragment homeChildFragment;
+
+    @BindView(R.id.home_child_frag_container)
+    View homeChildContainer;
 
     Unbinder unbinder;
 
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        AndroidInjection.inject(this);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,7 +59,8 @@ public class HomeFragment extends Fragment implements HomeView {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_home, container, false);
         unbinder = ButterKnife.bind(this, rootView);
-        presenter.onAttach(this);
+        addFragment();
+        //presenter.onAttach(this);
         return rootView;
     }
 
@@ -101,5 +123,27 @@ public class HomeFragment extends Fragment implements HomeView {
     public void onDestroyView() {
         super.onDestroyView();
         unbinder.unbind();
+    }
+
+    private void addFragment() {
+      /*  HomeChildFragment homeChildFragment = getHomeChildFragment();
+        if (homeChildFragment == null) {
+            homeChildFragment = HomeChildFragment.newInstance();
+            getFragmentManager().beginTransaction()
+                    .add(R.id.home_child_frag_container, homeChildFragment)
+                    .commit();
+        }*/
+        getFragmentManager().beginTransaction()
+                .add(R.id.home_child_frag_container, homeChildFragment)
+                .commit();
+    }
+
+    private HomeChildFragment getHomeChildFragment() {
+        return (HomeChildFragment) getFragmentManager().findFragmentByTag(FRAG_TAG_HOME_CHILD);
+    }
+
+    @Override
+    public AndroidInjector<Fragment> fragmentInjector() {
+        return fragmentInjector;
     }
 }
